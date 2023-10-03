238db9ebc9a6da3e2deef6c13ac68e78c86b17e7
Bit7eg <stonysyrop@gmail.com>
2023-10-03 19:18:56 +0700

    control panel added

--- pages/app.php
+++ pages/app.php
@@ -1,6 +1,11 @@
 <main>
-    <canvas></canvas>
+    <canvas id="glcanvas" tabindex="-1">
+        Похоже, этот браузер не поддерживает HTML5 элемент <code>&lt;canvas&gt;</code>.
+    </canvas>
     <section id="control-panel">
-        
+        <span>Function: </span><input id="function" type="text" value="test_1"/>
+        <span>Method: </span><input id="method" type="text" value="euler"/>
+        <span>N: </span><input id="intervals" type="number" value="10"/>
+        <button id="update">Update</button>
     </section>
 </main>
