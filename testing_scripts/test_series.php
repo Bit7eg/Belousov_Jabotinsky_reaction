1e01783d4668eb47c68d489186a928463302f989
Bit7eg <stonysyrop@gmail.com>
2023-10-18 10:18:31 +0700

    test series ready

--- testing_scripts/test_series.php
+++ testing_scripts/test_series.php
@@ -1,15 +1,36 @@
 <?php
-$test_configs = array(
-    [
-        "function" => "test-1",
-        "method" => "euler",
-        "intervals_number" => 20,
-    ],
-);
+$func_list = [
+    "test-1",
+    "test-2",
+    "test-3",
+    "test-4",
+    "test-5",
+    "test-6",
+];
+$methods_list = [
+    "euler",
+    "geer4",
+    "predictor-adams",
+    "predictor-iterations",
+    "predictor-only",
+    "runge-kutta3",
+    "runge-kutta4",
+    "runge-kutta5",
+];
 
 require "test_function.php";
 
-foreach ($test_configs as $config) {
-    test($config["function"], $config["method"], $config["intervals_number"]);
+foreach ($func_list as $function) {
+    test($function, "euler", 1, ["solution"]);
+    echo "check1";
+    foreach ($methods_list as $method) {
+        for ($i=64; $i < 2**20; $i*=2) { 
+            test($function, $method, $i, ["solve"]);
+        }
+        echo "check2";
+        test($function, $method, 1, ["errors"]);
+        echo "check3";
+    }
+    echo "check4";
 }
 ?>
